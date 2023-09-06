213746ebc80fa2a9918b36969152edd1d5bf0bb1
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2023-09-06 15:22:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/appart/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.appart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.appart.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
